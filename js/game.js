0c0125a898efd4bacb30add6a981d909d6e4101f
Sardar Balnour <balnour.sardar@gmail.com>
2024-02-22 10:44:26 +0330

    fetch data from API

--- js/game.js
+++ js/game.js
@@ -1,1 +1,20 @@
-console.log("game start");
+const loader = document.getElementById("loader");
+const container = document.getElementById("container");
+
+const URL =
+  "https://opentdb.com/api.php?amount=10&difficulty=medium&type=multiple";
+let formattedData = null;
+
+const fetchData = async () => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  formattedData = json;
+  start();
+};
+
+const start = () => {
+  loader.style.display = "none";
+  container.style.display = "block";
+};
+
+window.addEventListener("load", fetchData);
