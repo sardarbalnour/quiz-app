3272fe9354db28ec59eacd2abfb79c39587ab5fe
Sardar Balnour <balnour.sardar@gmail.com>
2024-03-08 16:39:56 +0330

    handle errors

--- js/game.js
+++ js/game.js
@@ -10,10 +10,10 @@ const scoreText = document.getElementById("score");
 const nextButton = document.getElementById("next-button");
 const finishButton = document.getElementById("finish-button");
 const questionNumber = document.getElementById("question-number");
+const error = document.getElementById("error");
 
 const CORRECT_BONUS = 10;
-const URL =
-  `https://opentdb.com/api.php?amount=10&difficulty=${level}&type=multiple`;
+const URL = `https://opentdb.com/api.php?amount=10&difficulty=${level}&type=multiple`;
 let formattedData = null;
 let questionIndex = 0;
 let correctAnswer = null;
@@ -21,10 +21,15 @@ let score = 0;
 let isAccepted = true;
 
 const fetchData = async () => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  formattedData = formatData(json.results);
-  start();
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
+    formattedData = formatData(json.results);
+    start();
+  } catch (err) {
+    loader.style.display = "none";
+    error.style.display = "block";
+  }
 };
 
 const start = () => {
